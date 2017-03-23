7ebacfe3789c683a77c6962678b1df5e256fbdb6
Anderson Rancan <anderson.rancan@gmail.com>
2017-03-23 22:45:41 +0100

    working with input controls

--- k4Xaml/ViewModels/MainViewModel.js
+++ k4Xaml/ViewModels/MainViewModel.js
@@ -4,7 +4,25 @@ var k4xaml;
 (function (k4xaml) {
     var MainViewModel = (function () {
         function MainViewModel() {
+            var _this = this;
             this.DisplayMessage = ko.observable("Hello developers");
+            this.DisplayValue = ko.observable(true);
+            this.FirstName = ko.observable("");
+            this.LastName = ko.observable("");
+            this.DisplayMessages = ko.observableArray([]);
+            this.Sports = ko.observableArray([]);
+            this.SelectedSport = ko.observable();
+            this.FullName = ko.computed(function () {
+                return _this.FirstName() + " " + _this.LastName();
+            });
+            this.DisplayMessages.push({ Message: 'Message 1' });
+            this.DisplayMessages.push({ Message: 'Message 2' });
+            this.DisplayMessages.push({ Message: 'Message 3' });
+            this.DisplayMessages.push({ Message: 'Message 4' });
+            this.Sports.push({ id: 1, Name: 'Baseball' });
+            this.Sports.push({ id: 2, Name: 'Football' });
+            this.Sports.push({ id: 3, Name: 'Basketball' });
+            this.Sports.push({ id: 4, Name: 'Soccer' });
         }
         return MainViewModel;
     }());
--- k4Xaml/ViewModels/MainViewModel.ts
+++ k4Xaml/ViewModels/MainViewModel.ts
@@ -1,8 +1,35 @@
 ﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
 /// <reference path="../scripts/typings/knockout/knockout.d.ts" />
 
+
 module k4xaml {
     export class MainViewModel {
         public DisplayMessage: KnockoutObservable<string> = ko.observable("Hello developers");
+        public DisplayValue: KnockoutObservable<boolean> = ko.observable(true);
+
+        public FirstName: KnockoutObservable<string> = ko.observable("");
+        public LastName: KnockoutObservable<string> = ko.observable("");
+        public FullName: KnockoutComputed<string>
+
+        public DisplayMessages: KnockoutObservableArray<any> = ko.observableArray([]);
+
+        public Sports: KnockoutObservableArray<any> = ko.observableArray([]);
+        public SelectedSport: KnockoutObservable<any> = ko.observable();
+
+        constructor() {
+            this.FullName = ko.computed(() => {
+                return this.FirstName() + " " + this.LastName();
+            })
+
+            this.DisplayMessages.push({ Message: 'Message 1' });
+            this.DisplayMessages.push({ Message: 'Message 2' });
+            this.DisplayMessages.push({ Message: 'Message 3' });
+            this.DisplayMessages.push({ Message: 'Message 4' });
+
+            this.Sports.push({ id: 1, Name: 'Baseball' });
+            this.Sports.push({ id: 2, Name: 'Football' });
+            this.Sports.push({ id: 3, Name: 'Basketball' });
+            this.Sports.push({ id: 4, Name: 'Soccer' });
+        }
     }
 }
