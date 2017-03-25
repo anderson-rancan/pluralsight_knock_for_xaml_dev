d1f5fcb07cd16156d4cdb749e15529e66e4d73a5
Anderson Rancan <anderson.rancan@gmail.com>
2017-03-26 00:22:14 +0100

    binding context and control flow

--- k4Xaml/ViewModels/MainViewModel.js
+++ k4Xaml/ViewModels/MainViewModel.js
@@ -26,12 +26,67 @@ var k4xaml;
             this.Sports.push({ id: 2, Name: 'Football' });
             this.Sports.push({ id: 3, Name: 'Basketball' });
             this.Sports.push({ id: 4, Name: 'Soccer' });
+            this.Person = new Person();
+            this.Person.FirstName("Indiana");
+            this.Person.LastName("Jones");
+            this.Person.Age(35);
+            var address = new Address();
+            address.Address("123 Main");
+            address.City("SomeCity");
+            address.State("NC");
+            address.Zip("12345");
+            this.Person.Address.push(address);
+            var address = new Address();
+            address.Address("124 River bend");
+            address.City("AnotherCity");
+            address.State("NC");
+            address.Zip("23233");
+            address.IsDeleted(true);
+            this.Person.Address.push(address);
+            var address = new Address();
+            address.Address("127 Mountain");
+            address.City("SmallCity");
+            address.State("NC");
+            address.Zip("556");
+            this.Person.Address.push(address);
         }
         MainViewModel.prototype.Save = function (data) {
             data.DisplayValueToSave(data.FirstNameToSave() + " " + data.LastNameToSave());
         };
+        MainViewModel.prototype.SavePerson = function () {
+            console.log("Save person here");
+        };
         return MainViewModel;
     }());
     k4xaml.MainViewModel = MainViewModel;
+    var Person = (function () {
+        function Person() {
+            this.FirstName = ko.observable("");
+            this.LastName = ko.observable("");
+            this.Age = ko.observable(0);
+            this.Address = ko.observableArray([]);
+        }
+        Person.prototype.DeleteAddress = function (address) {
+            var self = this;
+            var newArray = ko.utils.arrayFilter(self.Address(), function (item) { return item != address; });
+            self.Address(newArray);
+        };
+        Person.prototype.EditAddress = function (address) {
+            address.City(address.City() + " edited");
+        };
+        return Person;
+    }());
+    k4xaml.Person = Person;
+    var Address = (function () {
+        function Address() {
+            this.Address = ko.observable("");
+            this.City = ko.observable("");
+            this.State = ko.observable("");
+            this.Zip = ko.observable("");
+            this.IsDeleted = ko.observable(false);
+        }
+        return Address;
+    }());
+    k4xaml.Address = Address;
 })(k4xaml || (k4xaml = {}));
 //# sourceMappingURL=MainViewModel.js.map
--- k4Xaml/ViewModels/MainViewModel.ts
+++ k4Xaml/ViewModels/MainViewModel.ts
@@ -20,6 +20,8 @@ module k4xaml {
         public Sports: KnockoutObservableArray<any> = ko.observableArray([]);
         public SelectedSport: KnockoutObservable<any> = ko.observable();
 
+        public Person: Person;
+
         constructor() {
             this.FullName = ko.computed(() => {
                 return this.FirstName() + " " + this.LastName();
@@ -34,10 +36,68 @@ module k4xaml {
             this.Sports.push({ id: 2, Name: 'Football' });
             this.Sports.push({ id: 3, Name: 'Basketball' });
             this.Sports.push({ id: 4, Name: 'Soccer' });
+
+            this.Person = new Person();
+            this.Person.FirstName("Indiana");
+            this.Person.LastName("Jones");
+            this.Person.Age(35);
+
+            var address = new Address();
+            address.Address("123 Main");
+            address.City("SomeCity");
+            address.State("NC");
+            address.Zip("12345");
+            this.Person.Address.push(address);
+
+            var address = new Address();
+            address.Address("124 River bend");
+            address.City("AnotherCity");
+            address.State("NC");
+            address.Zip("23233");
+            address.IsDeleted(true);
+            this.Person.Address.push(address);
+
+            var address = new Address();
+            address.Address("127 Mountain");
+            address.City("SmallCity");
+            address.State("NC");
+            address.Zip("556");
+            this.Person.Address.push(address);
         }
 
         public Save(data) {
             data.DisplayValueToSave(data.FirstNameToSave() + " " + data.LastNameToSave());
         }
+
+        public SavePerson() {
+            console.log("Save person here");
+        }
     }
+
+    export class Person {
+        public FirstName: KnockoutObservable<string> = ko.observable("");
+        public LastName: KnockoutObservable<string> = ko.observable("");
+        public Age: KnockoutObservable<number> = ko.observable(0);
+        public Address: KnockoutObservableArray<any> = ko.observableArray([]);
+
+        public DeleteAddress(address: Address) {
+            var self = this;
+
+            var newArray = ko.utils.arrayFilter(self.Address(), (item) => { return item != address; });
+            self.Address(newArray);
+        }
+
+        public EditAddress(address: Address) {
+            address.City(address.City() + " edited");
+        }
+    }
+
+    export class Address {
+        public Address: KnockoutObservable<string> = ko.observable("");
+        public City: KnockoutObservable<string> = ko.observable("");
+        public State: KnockoutObservable<string> = ko.observable("");
+        public Zip: KnockoutObservable<string> = ko.observable("");
+        public IsDeleted: KnockoutObservable<boolean> = ko.observable(false);
+    }
+
 }
